807c05bbac8f9c2e9397b8c5d986f831ac905931
Will Bryant <will.bryant@gmail.com>
2016-12-19 23:20:13 +1300

    put two repos in the seed to get us thinking about how this works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Project.transaction do
   project = Project.create!(name: "All Cing I")
@@ -12,6 +7,9 @@ Project.transaction do
 
   repository = Repository.create!(uri: "git@domain.com:somewhere/your_repo.git")
   container = standard.components.create!(repository: repository, container_name: "db")
+
+  memcache_repository = Repository.create!(uri: "git@domain.com:somewhere/your_memcache_repo.git")
+  memcache_container = standard.components.create!(repository: memcache_repository, container_name: "memcache")
 
   EnqueueConfigurationBuild.new(standard).call({})
 end
