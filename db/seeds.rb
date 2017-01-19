149f946024fb76638a6dfd79ff4103e79d741f6b
Will Bryant <will.bryant@gmail.com>
2017-01-19 21:36:04 +1300

    configure self-test as the seeds.  take more of the database.yml options as ENV vars.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,15 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 Project.transaction do
-  project = Project.create!(name: "All Cing I")
-  standard = project.configurations.create!(name: "Standard")
+  # we self-host tests for this project itself and use that as our seeds
+  project = Project.find_or_create_by!(name: "All Cing I")
+  test_with_mysql = project.configurations.find_or_create_by!(name: "Self-test")
 
-  repository = Repository.create!(uri: "git@domain.com:somewhere/your_repo.git")
-  container = standard.components.create!(repository: repository, container_name: "db")
+  allci_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci.git")
+  allci_component = test_with_mysql.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
 
-  memcache_repository = Repository.create!(uri: "git@domain.com:somewhere/your_memcache_repo.git")
-  memcache_container = standard.components.create!(repository: memcache_repository, container_name: "memcache")
+  mysql_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci-test-mysql.git")
+  mysql_container = test_with_mysql.components.find_or_create_by!(repository: mysql_repository, container_name: "mysql")
 
-  EnqueueConfigurationBuild.new(standard).call({})
+  EnqueueConfigurationBuild.new(test_with_mysql).call({})
 end
