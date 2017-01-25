636a15a733a85908ac3f25cd974e1e6a040a7ca8
Will Bryant <will.bryant@gmail.com>
2017-01-26 00:04:49 +1300

    add a second configuration to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,22 @@
 Project.transaction do
   # we self-host tests for this project itself and use that as our seeds
   project = Project.find_or_create_by!(name: "All Cing I")
-  test_with_mysql = project.configurations.find_or_create_by!(name: "Self-test")
 
   allci_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci.git")
-  allci_component = test_with_mysql.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
-  allci_component.component_variables.find_or_create_by!(name: "ALLCI_DATABASE_SERVER", value: "db")
-
   mysql_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci-test-mysql.git")
+  mariadb_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci-test-mariadb.git")
+
+  test_with_mysql = project.configurations.find_or_create_by!(name: "Self-test with mysql")
+  test_with_mariadb = project.configurations.find_or_create_by!(name: "Self-test with mariadb")
+
   mysql_container = test_with_mysql.components.find_or_create_by!(repository: mysql_repository, container_name: "db")
+  allci_with_mysql_component = test_with_mysql.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
+  allci_with_mysql_component.component_variables.find_or_create_by!(name: "ALLCI_DATABASE_SERVER", value: "db")
+
+  mariadb_container = test_with_mariadb.components.find_or_create_by!(repository: mariadb_repository, container_name: "db")
+  allci_with_mysql_component = test_with_mariadb.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
+  allci_with_mysql_component.component_variables.find_or_create_by!(name: "ALLCI_DATABASE_SERVER", value: "db")
 
   EnqueueConfigurationBuild.new(test_with_mysql).call({})
+  EnqueueConfigurationBuild.new(test_with_mariadb).call({})
 end
