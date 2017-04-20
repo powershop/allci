51d57aa398d72c0473b54e0e79b7ebfaabd7a132
Will Bryant <will.bryant@gmail.com>
2017-04-20 13:29:58 +1200

    use public checkout URLs for the build

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ Project.transaction do
   # we self-host tests for this project itself and use that as our seeds
   project = Project.find_or_create_by!(name: "All Cing I")
 
-  allci_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci.git")
-  mysql_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci-test-mysql.git")
-  mariadb_repository = Repository.find_or_create_by!(uri: "git@github.com:powershop/allci-test-mariadb.git")
+  allci_repository = Repository.find_or_create_by!(uri: "https://github.com/powershop/allci.git")
+  mysql_repository = Repository.find_or_create_by!(uri: "https://github.com/powershop/allci-test-mysql.git")
+  mariadb_repository = Repository.find_or_create_by!(uri: "https://github.com/powershop/allci-test-mariadb.git")
 
   test_with_mysql = project.configurations.find_or_create_by!(name: "Self-test with mysql")
   test_with_mariadb = project.configurations.find_or_create_by!(name: "Self-test with mariadb")
@@ -16,10 +16,12 @@ Project.transaction do
   db_component = test_with_mysql.components.find_or_create_by!(repository: mysql_repository, container_name: "db")
   allci_component = test_with_mysql.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
   allci_component.component_variables.find_or_create_by!(name: "ALLCI_DATABASE_SERVER", value: "db")
+  allci_component.component_variables.find_or_create_by!(name: "BUILD_RAKE_TASKS", value: "default")
 
   db_component = test_with_mariadb.components.find_or_create_by!(repository: mariadb_repository, container_name: "db")
   allci_component = test_with_mariadb.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
   allci_component.component_variables.find_or_create_by!(name: "ALLCI_DATABASE_SERVER", value: "db")
+  allci_component.component_variables.find_or_create_by!(name: "BUILD_RAKE_TASKS", value: "default")
 
   allci_component = stats.components.find_or_create_by!(repository: allci_repository, container_name: "allci")
   allci_component.component_variables.find_or_create_by!(name: "BOOTSTRAP_RAKE_TASKS", value: "")
