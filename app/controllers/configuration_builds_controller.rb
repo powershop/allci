d49780a481c5fb44ccaaaa3098abeab6f8c3ce29
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2017-05-17 19:32:29 +1200

    Initial scratching at UI to display build builddown

--- app/controllers/configuration_builds_controller.rb
+++ app/controllers/configuration_builds_controller.rb
@@ -1,10 +1,27 @@
 class ConfigurationBuildsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
+    @configuration_builds = ConfigurationBuild.all.order(:created_at)
     respond_to do |format|
-      format.json { render json: ConfigurationBuild.all }
+      format.json { render json: @configuration_builds }
+      format.html { render layout: 'simple' }
     end
   end
 
   def show
+    @configuration_build = ConfigurationBuild.find(params[:id])
+    @build_task_runs = @configuration_build.build_task_runs.includes(:build_task)
+
+    @start_time = @build_task_runs.min_by(&:started_at).started_at
+    end_time = @build_task_runs.max_by(&:finished_at).finished_at
+
+    @runtime = end_time - @start_time
+
+    @build_task_runs_by_runner = @build_task_runs.group_by(&:runner_id)
+
+    respond_to do |format|
+      format.html { render layout: false}
+    end
   end
 end
