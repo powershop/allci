b3c0511765d9e538863271f255f70ba2833bbfd0
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2017-05-18 18:24:20 +1200

    Add Burndowns controller
    Special burndowns controller based on what we were doing in
    configuration_builds_controller.
    
    Found at:
      /burndowns
    Accepts a time scale (minutes):
      /burndowns?scale=5
    Accepts an optional ConfigurationBuild to base display off:
      /burndowns?config_build_id=7
    
    Display also shows vertical lines marking marking each minute of run
    time.

---
+++ app/controllers/burndowns_controller.rb
@@ -0,0 +1,39 @@
+class BurndownsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @configuration_builds = ConfigurationBuild.all.order(:created_at)
+    respond_to do |format|
+      format.json { render json: @configuration_builds }
+      format.html { render layout: 'simple' }
+    end
+  end
+
+  def show
+    if params[:config_build_id]
+      @configuration_build = ConfigurationBuild.find(params[:config_build_id])
+    else
+      @configuration_build = ConfigurationBuild.last
+    end
+    @build_task_runs = @configuration_build.build_task_runs.includes(:build_task)
+
+    @start_time = @build_task_runs.min_by(&:started_at).started_at
+    end_time = @build_task_runs.max_by(&:finished_at).finished_at
+
+    @runtime = end_time - @start_time
+
+    scale = if params[:scale]
+              params[:scale].to_d.minutes
+            else
+              ((@runtime/60) + 1).to_i.minutes
+            end
+
+    @scale_time = scale
+
+    @build_task_runs_by_runner = @build_task_runs.joins(:runner).order("runners.name").preload(:runner).group_by(&:runner)
+
+    respond_to do |format|
+      format.html { render layout: false}
+    end
+  end
+end
