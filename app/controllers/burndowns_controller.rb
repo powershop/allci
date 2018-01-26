d5b762a5c88a2e12a1338483dd7f09e21b280c67
Michael Fowler <mkrfowler@gmail.com>
2018-01-26 15:59:13 +1300

    Restore link functionality to burndown pages

--- app/controllers/burndowns_controller.rb
+++ app/controllers/burndowns_controller.rb
@@ -12,14 +12,6 @@ class BurndownsController < ApplicationController
 
     @timeframe = start_time..(start_time + duration)
 
-    runs_by_runner_and_config = BuildTaskRun.
-      where(started_at: @timeframe).
-      left_joins(:runner, :configuration_build, :build_task).
-      select('build_task_runs.*, build_tasks.stage AS inferred_stage, runners.name AS inferred_runner_name, configuration_builds.id AS configuration_build_id')
-
-    # puts "time to load: #{Benchmark.ms { runs_by_runner_and_config.length }}"
-    # puts "time to group: #{Benchmark.ms { runs_by_runner_and_config = runs_by_runner_and_config.group_by { |run| [run.inferred_runner_name, run.configuration_build_id] } }}"
-
     runs_by_runner_and_config = Projected.burndown_rows_for_timeframe(@timeframe).group_by { |run| [run.name, run.configuration_build_id] }
 
     @data = {}
@@ -44,12 +36,14 @@ class BurndownsController < ApplicationController
 
   class Projected
     PROJECTION = [
+      %i[build_task_runs      id         ],
+      %i[build_task_runs      started_at ],
       %i[build_task_runs      finished_at],
-      %i[build_task_runs      started_at],
-      %i[build_task_runs      state ],
-      %i[build_tasks          stage        ],
-      %i[runners              name    ],
-      %i[configuration_builds id    configuration_build_id]
+      %i[build_task_runs      state      ],
+      %i[build_tasks          stage      ],
+      %i[build_tasks          task       ],
+      %i[runners              name       ],
+      %i[configuration_builds id         configuration_build_id]
     ]
 
     def self.burndown_rows_for_timeframe(timeframe)
@@ -73,6 +67,19 @@ class BurndownsController < ApplicationController
 
       def relative_start(start_time_of_build)
         started_at - start_time_of_build
+      end
+
+      def label
+        lead     = task&.split(' ')&.last
+        duration = "(#{duration.to_i}s - #{started_at})" + " - ID #{id}"
+
+        [lead, duration].compact.join(' ')
+      end
+
+      def short_label
+        return nil unless task
+
+        task.split(' ').last.split('/').last + " (#{duration.to_i}s)"
       end
     end
   end
